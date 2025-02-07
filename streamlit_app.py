72dd951f41b294c036071ab4126d6d3d1f0f9ed8
govindjoshisnowflake <govind.joshi@exusia.com>
2025-02-07 13:42:49 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,11 +31,11 @@ if ingredient_list:
          
         st.text(ingredients_string)
 
-     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,NAME_ON_ORDER )
+        my_insert_stmt = """ insert into smoothies.public.orders(ingredients,NAME_ON_ORDER )
             values ('""" + ingredients_string + "','" + name_on_order + """')"""
 
-     #st.write(my_insert_stmt)
-     time_to_insert=st.button("Submit Order!")
-     if time_to_insert:
-        session.sql(my_insert_stmt).collect()
-        st.success('Your Smoothie is ordered,' + name_on_order +'!', icon="✅")
+    #st.write(my_insert_stmt)
+time_to_insert=st.button("Submit Order!")
+if time_to_insert:
+    session.sql(my_insert_stmt).collect()
+    st.success('Your Smoothie is ordered,' + name_on_order +'!', icon="✅")
